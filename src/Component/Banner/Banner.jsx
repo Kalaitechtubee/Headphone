bfa994037d324930ae262691ee4f07d7d1896d68
Kalaitechtubee <145133387+Kalaitechtubee@users.noreply.github.com>
2024-11-25 11:35:37 +0530

    Update Banner.jsx

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -15,12 +15,12 @@ const Banner = () => {
     <>
       <section>
         <div className='container py-14 grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 gap-12'>
-          {/* Banner image */}
+          
           <div data-aos="fade-right" data-aos-duration="1000">
             <img src={headphoneImage4} alt="Headphones" className='w-[300px] md:w-[400px]' />
           </div>
 
-          {/* Banner info */}
+         
           <div className='flex flex-col justify-center'>
             <div className='text-center md:text-left space-y-4 lg:max-w-[450px]' data-aos="fade-left">
               <h1 className='text-3xl lg:text-4xl font font-poppins font-semibold'>
@@ -30,7 +30,7 @@ const Banner = () => {
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam minus laboriosam velit dolores sapiente magnam laudantium ab, eum facere officia excepturi voluptatem exercitationem dolorum nobis cupiditate error, vel delectus recusandae.
               </p>
 
-              {/* Button with AOS fade-up effect */}
+              
               <button
                 className='border-2 border-[#e33343] text-[#e33343] px-6 py-2 rounded-md hover:bg-[#e33343] hover:text-white'
                 data-aos="fade-up"
